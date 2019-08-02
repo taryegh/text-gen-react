245c7fd7bfcb58ea77e1a1382e70883a3f4e3165
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-03 03:24:59 +0400

    Updates

---
+++ precache-manifest.616932e37f9ec4966c0c74e52432c756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed78b0d7d9dd7f1c7c38f3e65f9bc6b",
+    "url": "/meme-gen-react/index.html"
+  },
+  {
+    "revision": "9f3dfddc54bb9908e78e",
+    "url": "/meme-gen-react/static/css/main.153bc09c.chunk.css"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/meme-gen-react/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "9f3dfddc54bb9908e78e",
+    "url": "/meme-gen-react/static/js/main.af35d1f1.chunk.js"
+  },
+  {
+    "revision": "484406dbe622c9b53b11",
+    "url": "/meme-gen-react/static/js/runtime~main.56fc6bb3.js"
+  }
+]);
